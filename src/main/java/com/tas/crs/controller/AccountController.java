48b0469a004d7f90712a1b1252a28ff0aa952453
NC-52 <59743364+NC-52@users.noreply.github.com>
2021-07-13 14:33:58 +0100

    Update AccountController.java

--- src/main/java/com/tas/crs/controller/AccountController.java
+++ src/main/java/com/tas/crs/controller/AccountController.java
@@ -2,11 +2,31 @@ package com.tas.crs.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.tas.crs.service.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/accounts")
 public class AccountController {
 
     // TODO: implement all account's methods
-
+    private final AccountServiceImpl mAccountService;
+    
+    public AccountController(AccountService Impl accountService) {
+        mAccountService = accountService;
+    }
+      
+    @GetMapping
+    public ResponseEntity<List<<Account>> getAllAccounts() {
+        return new ResponseEntity<>(mAccountService.fetchAccounts(), OK);
+    }
+    
+    @GetMapping(path ="/{id}")
+    public ResponseEntity<Account> getAccount(final @PathVariable(name = "id") Long accountId) {
+        Account account = mAccountService
+            .fetchAccount(accountId);
+        return ResponseEntity.badRequest().body(account);
+    }
 }
